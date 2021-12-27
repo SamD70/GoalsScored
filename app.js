fad4a5987565a1d28e43eca1e78af34d3e38a926
SamD70 <77805881+SamD70@users.noreply.github.com>
2021-12-27 20:39:24 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+
+const btn = document.querySelector('button');
+const goalsOutput = document.querySelector('.goals');
+
+
+btn.addEventListener('click', test);
+
+function test(){
+    const teamInput = document.querySelector('.teamname').value;
+    xhr = new XMLHttpRequest
+    xhr.open('GET', 'https://s3.eu-west-1.amazonaws.com/hackajob-assets1.p.hackajob/challenges/football_session/football.json', false);
+    
+    //const data = xhr.responseText.JSON.parse();
+    xhr.send();
+
+    //let data = xhr.JSON.parse();
+    if( xhr.status === 200){
+        totalGoals(teamInput);
+        
+    }
+
+}
+
+function totalGoals(teamName){
+    const data = JSON.parse(xhr.responseText);
+    let goals = 0;
+    for (let i =0; i<38; i++){
+        for(let a = 0; a< 10; a++){
+            let obj = data.rounds[i].matches[a];
+            if(obj.team1.key === teamName){
+                goals +=  obj.score1;
+            }
+            else if(obj.team2.key === teamName){
+                goals +=  obj.score2;
+            }
+            
+        }
+        
+    }
+    goalsOutput.innerHTML = goals;
+}
